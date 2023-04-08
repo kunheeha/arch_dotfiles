6f57bf460324214d7b10e14180fd63c07ee0fdd1
Kunhee Ha <kunheeha@gmail.com>
2023-04-08 20:34:11 +0100

    script to set wallpaper

--- scripts/set_wallpaper.sh
+++ scripts/set_wallpaper.sh
@@ -1,3 +1,14 @@
-#!/bin/zsh
+#!/bin/bash
 
-swww img ~/Wallpapers/Nord/nord_town.png -t random
+WALLDIR=~/Wallpapers
+
+if [[ "$1" == 'nord' ]]; then
+  WALLDIR=$WALLDIR/Nord
+elif [[ "$1" == 'mocha' ]]; then
+  WALLDIR=$WALLDIR/Catppuccin
+else
+  WALLDIR=$(find $WALLDIR -maxdepth 1 -type d | tail -n 2 | sort -R | tail -n 1)
+fi
+
+WALLPAPER=$(ls $WALLDIR/ | sort -R | tail -n 1)
+swww img $WALLDIR/$WALLPAPER -t random
