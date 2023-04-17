fef48b99170890f905644c89eda9a388c54a42f3
Kunhee Ha <kunheeha@gmail.com>
2023-04-17 11:33:19 +0100

    some aesthetic changes to volume notification

--- scripts/volume.sh
+++ scripts/volume.sh
@@ -3,15 +3,24 @@
 send_volume_notification ()
 {
   VOLUME=$(pamixer --get-volume)
-  VOLUMEBAR=$(seq -s "━" $(($VOLUME/5)) | sed 's/[0-9]//g')
-  if [[ "$VOLUME" -le "30" ]]; then
-    icon="/usr/share/icons/Faba/48x48/notifications/notification-audio-volume-low.svg"
-  elif [[ "$VOLUME" -le "70" ]]; then
-    icon="/usr/share/icons/Faba/48x48/notifications/notification-audio-volume-medium.svg"
+  if [[ "$VOLUME" -lt "10" ]]; then
+    VOLUMEBAR=""
+    if [[ "$VOLUME" -eq "0" ]]; then
+      ICON="/usr/share/icons/Faba/48x48/notifications/notification-audio-volume-muted.svg"
+    else
+      ICON="/usr/share/icons/Faba/48x48/notifications/notification-audio-volume-low.svg"
+    fi
   else
-    icon="/usr/share/icons/Faba/48x48/notifications/notification-audio-volume-high.svg"
+    VOLUMEBAR=$(seq -s "━" $(($VOLUME/5)) | sed 's/[0-9]//g')
+    if [[ "$VOLUME" -le "30" ]]; then
+      ICON="/usr/share/icons/Faba/48x48/notifications/notification-audio-volume-low.svg"
+    elif [[ "$VOLUME" -le "70" ]]; then
+      ICON="/usr/share/icons/Faba/48x48/notifications/notification-audio-volume-medium.svg"
+    else
+      ICON="/usr/share/icons/Faba/48x48/notifications/notification-audio-volume-high.svg"
+    fi
   fi
-  notify-send -i "$icon" "$VOLUMEBAR" "$VOLUME" -r 1
+  notify-send -i "$ICON" "$VOLUME    $VOLUMEBAR" "" -r 1
 }
 
 case "$1" in
