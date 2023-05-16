ef02aa81c10590b680a040eb4ca1dd42e56448bd
Kunhee Ha <kunheeha@gmail.com>
2023-05-16 16:16:26 +0100

    cleanup aliases file and add alias to connect and disconnect airpods

--- .config/aliases/aliases.zsh
+++ .config/aliases/aliases.zsh
@@ -1,28 +1,38 @@
+# ##################
+# OPENING FILES
+# ##################
 
 #xdg-open
 alias open='xdg-open'
 # use Neovim
 alias vim='nvim'
+
+# ##################
+# COMMON COMMANDS
+# ##################
+
 # pacman
 alias pacman='sudo pacman'
-# use bat instead of cat
-# alias cat='bat'
+# Quick Navigation
+alias projects='cd ~/Projects'
 # use lsd instead of vanilla ls
 alias ls='lsd'
 alias la='lsd -a'
 alias ll='lsd -la'
 alias lt='lsd --tree'
-# use ptsh cp for status bar
-# alias cp='ptcp'
 
-# Change aesthetics
+# ##################
+# AESTHETICS
+# ##################
+
+# Wallpaper
+alias wallpaper='~/.config/scripts/set_wallpaper.sh'
 
 # Change alacritty opacity
 alias opacity='python ~/.config/scripts/change_alacritty_opacity.py'
+
 # Change alacritty theme
 alias theme='python ~/.config/scripts/change_alacritty_theme.py'
-# Default wallpaper
-alias wallpaper='~/.config/scripts/set_wallpaper.sh'
 
 # Change theme and opacity
 alias nord='python ~/.config/scripts/change_alacritty_opacity.py 1.0 && python ~/.config/scripts/change_alacritty_theme.py nord && python ~/.config/scripts/change_nvim_theme.py nord'
@@ -31,11 +41,13 @@ alias kana='python ~/.config/scripts/change_alacritty_opacity.py 1.0 && python ~
 alias glacier='python ~/.config/scripts/change_alacritty_opacity.py 0.9 && python ~/.config/scripts/change_alacritty_theme.py cat && python ~/.config/scripts/change_nvim_theme.py glacier'
 alias solar='python ~/.config/scripts/change_alacritty_opacity.py 0.8 && python ~/.config/scripts/change_alacritty_theme.py solar && python ~/.config/scripts/change_nvim_theme.py solar && ~/.config/scripts/set_wallpaper.sh solar'
 alias light='python ~/.config/scripts/change_alacritty_opacity.py 1.0 && python ~/.config/scripts/change_alacritty_theme.py light && python ~/.config/scripts/change_nvim_theme.py light && ~/.config/scripts/set_wallpaper.sh solar'
-
 alias poi='python ~/.config/scripts/change_alacritty_opacity.py 0.9 && python ~/.config/scripts/change_alacritty_theme.py poi'
 alias storm='python ~/.config/scripts/change_alacritty_opacity.py 1.0 && python ~/.config/scripts/change_alacritty_theme.py storm'
 
-# Git
+# ##################
+# GIT
+# ##################
+
 alias gls='git worktree list'
 alias gA='git add .'
 alias ga='git add'
@@ -54,15 +66,28 @@ alias gwa='git worktree add'
 alias gd='git diff'
 alias gdc='git diff --cached'
 
-# Tmux
+# ##################
+# TMUX
+# ##################
+
 alias tma='tmux attach -t'
 alias tmn='tmux new -s'
 alias tls='tmux ls'
 
-# Quick Navigation
-alias projects='cd ~/Projects'
+# ##################
+# TOOLS
+# ##################
 
 # ScreenRecord
 alias screenrecord='wf-recorder -g "$(slurp)" -f'
+
 # Music
 alias music="ncmpcpp"
+
+# ##################
+# DEVICES
+# ##################
+
+# Airpods
+alias airpods='bluetoothctl connect 40:E6:4B:84:22:17'
+alias airpodsoff='bluetoothctl disconnect 40:E6:4B:84:22:17'
