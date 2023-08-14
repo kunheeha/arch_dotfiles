14161328caa2caf743f8946da7221ee7f72d18d8
Kunhee Ha <kunheeha@gmail.com>
2023-08-14 08:52:23 +0100

    rose pine theme

--- scripts/set_wallpaper.sh
+++ scripts/set_wallpaper.sh
@@ -10,6 +10,10 @@ elif [[ "$1" == 'solar' ]]; then
   WALLDIR=$WALLDIR/Solarized
 elif [[ "$1" == 'dark' ]]; then
   WALLDIR=$WALLDIR/Dark
+elif [[ "$1" == 'animated' ]]; then
+  WALLDIR=$WALLDIR/animated
+elif [[ "$1" == 'rose' ]]; then
+  WALLDIR=$WALLDIR/Rose
 else
   WALLDIR=$(find $WALLDIR -maxdepth 1 -type d | tail -n 2 | sort -R | tail -n 1)
 fi
