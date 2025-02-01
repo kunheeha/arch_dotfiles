1a798422169e76a6aedfa6fdc71500f216f670dc
Kunhee Ha <kunheeha@gmail.com>
2025-02-01 04:19:03 +0000

    move aesthetics related aliases to separate file

--- .config/aliases/aliases.zsh
+++ .config/aliases/aliases.zsh
@@ -28,24 +28,7 @@ alias fd='cd $(find * -type d | fzf)'
 # AESTHETICS
 # ##################
 
-# Wallpaper
-alias wallpaper='~/.config/scripts/set_wallpaper.sh'
-
-# Change alacritty opacity
-alias opacity='python ~/.config/scripts/change_alacritty_opacity.py'
-
-# Change alacritty theme
-alias theme='python ~/.config/scripts/change_alacritty_theme.py'
-
-# Change theme and opacity
-alias nord='python ~/.config/scripts/change_alacritty_opacity.py 1.0 && python ~/.config/scripts/change_alacritty_theme.py nord && python ~/.config/scripts/change_nvim_theme.py nord'
-alias mocha='python ~/.config/scripts/change_alacritty_opacity.py 0.8 && python ~/.config/scripts/change_alacritty_theme.py cat && python ~/.config/scripts/change_nvim_theme.py cat'
-alias kana='python ~/.config/scripts/change_alacritty_opacity.py 1.0 && python ~/.config/scripts/change_alacritty_theme.py kana && python ~/.config/scripts/change_nvim_theme.py kana'
-alias glacier='python ~/.config/scripts/change_alacritty_opacity.py 0.9 && python ~/.config/scripts/change_alacritty_theme.py cat && python ~/.config/scripts/change_nvim_theme.py glacier'
-alias solar='python ~/.config/scripts/change_alacritty_opacity.py 0.8 && python ~/.config/scripts/change_alacritty_theme.py solar && python ~/.config/scripts/change_nvim_theme.py solar && ~/.config/scripts/set_wallpaper.sh solar'
-alias light='python ~/.config/scripts/change_alacritty_opacity.py 1.0 && python ~/.config/scripts/change_alacritty_theme.py light && python ~/.config/scripts/change_nvim_theme.py light && ~/.config/scripts/set_wallpaper.sh solar'
-alias poi='python ~/.config/scripts/change_alacritty_opacity.py 0.9 && python ~/.config/scripts/change_alacritty_theme.py poi'
-alias storm='python ~/.config/scripts/change_alacritty_opacity.py 1.0 && python ~/.config/scripts/change_alacritty_theme.py storm'
+source ~/.config/aliases/aesthetics.zsh
 
 # ##################
 # GIT
