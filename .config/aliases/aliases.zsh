184c48613313dcb46e6ed4551e3feddf7ce7dad4
Kunhee Ha <kunheeha@gmail.com>
2026-03-14 12:55:59 +0000

    git worktree related scrips and aliases

--- .config/aliases/aliases.zsh
+++ .config/aliases/aliases.zsh
@@ -37,7 +37,6 @@ source ~/.config/aliases/aesthetics.zsh
 # GIT
 # ##################
 
-alias gls='git worktree list'
 alias gA='git add .'
 alias ga='git add'
 alias gc='git commit -m'
@@ -51,11 +50,17 @@ alias grc='git rebase --continue'
 alias gm='git merge'
 alias gmc='git merge --continue'
 alias gf='git fetch'
-alias gwa='git worktree add'
-alias gwd='pwd | xargs ~/.config/scripts/git_worktree_remove_current.sh; cd ..'
 alias gd='git diff'
 alias gdc='git diff --cached'
 alias gcp='git cherry-pick'
+
+# Git Worktree Specific
+alias gls='git worktree list'
+alias gwa='git worktree add'
+alias gwd='pwd | xargs ~/.config/scripts/git_worktree_remove_current.sh; cd ..'
+alias gwcheck='pwd | xargs ~/.config/scripts/git_worktree_check_setup.sh'
+alias gwfetch='git --git-dir=$(pwd) fetch origin'
+alias gwaremote='~/.config/scripts/git_worktree_add_remote_branch.sh'
 
 # ##################
 # TMUX
